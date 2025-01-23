16a80a7ef673cf1661d03074cc692ef4ea26e50d
mrcolorr <mattiar11@gmail.com>
2025-01-23 18:49:29 +0100

    added tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ echo "Updating package list..."
 sudo apt update
 
 # Install Python 3.9 and venv
-echo "Installing Python 3.9 and its venv..."
-sudo apt install -y python3.9 python3.9-venv tmux
+echo "Installing packages..."
+sudo apt install -y  tmux
 
-echo "Python 3.9 and virtual environment setup complete!"
+echo "Packages installed!"
